3ad1346b5353e6c14a405ee21b907f9b579d04ca
FhatimaReyes <fhareal10@gmail.com>
2023-02-14 10:06:16 -0600

    add requirements.txt

--- names-search.py
+++ names-search.py
@@ -7,14 +7,14 @@ DATA.URL = "dataset.csv"
 @st.cache 
 def load_data_byname(name):
     data= pd.read_csv(DATA_URL) # read csv
-    filtered_data_byname = data["name"].str.constains(name)]
+    filtered_data_byname = data[data["name"].str.contains(name)]
     return filtered_data_byname # return the dataframe
 
 myname = st.text_input("nombre : ")
 if (myname)
     filteredbyname = load_data_byname(myname) # call the function
     count_row = filteredbyname.shape[0] 
-    st.write(f"Total names : {count_row")
+    st.write(f"Total names : {count_row}")
 
     st.dataframe(filterbyname)
 
