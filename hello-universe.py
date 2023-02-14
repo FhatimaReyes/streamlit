7a1bf8dc8f13cbcd98d8bf0c707ace4bc92a0b73
FhatimaReyes <fhareal10@gmail.com>
2023-02-14 09:58:33 -0600

    add python streamlit programs

---
+++ hello-universe.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title('My first app')
+st.header('My first app')
+ 
+st.write("""
+    # Ejemplo Simple de una App de Predicción
+     Hello universe 
+
+     ¡Esta app predice mis datos!
+
+     """)
