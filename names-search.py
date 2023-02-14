b6e781e3584f80b259779a7b5f5f6cf59e67c8f1
FhatimaReyes <fhareal10@gmail.com>
2023-02-14 10:21:29 -0600

    update names-search.py

--- names-search.py
+++ names-search.py
@@ -2,18 +2,18 @@ import streamlit as st
 import pandas as pd
 
 st.title("search names")
-DATA.URL = "dataset.csv"
+DATA_URL = "dataset.csv"
 
-@st.cache 
+@st.cache
 def load_data_byname(name):
     data= pd.read_csv(DATA_URL) # read csv
     filtered_data_byname = data[data["name"].str.contains(name)]
     return filtered_data_byname # return the dataframe
 
-myname = st.text_input("nombre : ")
-if (myname)
-    filteredbyname = load_data_byname(myname) # call the function
-    count_row = filteredbyname.shape[0] 
+myname = st.text_input("nombre :")
+if (myname):
+    filterbyname = load_data_byname(myname) # call the function
+    count_row = filterbyname.shape[0]
     st.write(f"Total names : {count_row}")
 
     st.dataframe(filterbyname)
